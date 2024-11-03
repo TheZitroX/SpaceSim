42dfa2d7d1ebe37e97b2e769e3480e4a8a7b1df0
John Lienau <develop@johnlienau.de>
2024-11-03 21:09:08 +0100

    init

---
+++ base/base.c
@@ -0,0 +1,3 @@
+//
+// Created by zitri on 03.11.2024.
+//
---
+++ base/defines.h
@@ -0,0 +1,8 @@
+//
+// Created by zitri on 03.11.2024.
+//
+
+#ifndef SPACESIM_DEFINES_H
+#define SPACESIM_DEFINES_H
+
+#endif //SPACESIM_DEFINES_H
