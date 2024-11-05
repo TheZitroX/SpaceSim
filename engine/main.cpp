c465184ef96a685c570b10013fa7292946d8590e
John Lienau <lienaujohnny@gmail.com>
2024-11-05 14:26:27 +0100

    first sdl window on windows

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,31 +1,52 @@
-#include <iostream>
 
-// #include <defines.h>
 
 #include <SDL3/SDL.h>
+#include <iostream>
+
 
 int main() {
-  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-    std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
     return 1;
   }
 
-  SDL_Window *window = SDL_CreateWindow(
-      "Hello World!",
-      640,
-      480,
-      SDL_WINDOW_FULLSCREEN
-  );
+  bool isFullscreen = true;
+  auto window = SDL_CreateWindow("Hello, SDL3!", 800, 600,
+                            SDL_WINDOW_FULLSCREEN | SDL_WINDOW_RESIZABLE);
 
-  if (window == nullptr) {
-    std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-    SDL_Quit();
+  if (!window) {
+    std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << std::endl;
     return 1;
   }
 
-  SDL_Delay(3000);
+  auto renderer = SDL_CreateRenderer(window, "Hello");
 
-  SDL_Quit();
+  auto running = true;
+  while (running) {
+    // vsync
+    auto start = SDL_GetTicks();
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+        auto end = SDL_GetTicks();
+        auto elapsed = end - start;
+        if (elapsed < 16) {
+          SDL_Delay(16 - elapsed);
+        }
+
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+        running = false;
+      } else if (event.key.scancode == SDL_SCANCODE_F11) {
+        if (event.key.down) {
+          isFullscreen = !isFullscreen;
+          SDL_SetWindowFullscreen(window, isFullscreen);
+        }
+      }
+    }
+  }
+
+
 
   return 0;
 }
